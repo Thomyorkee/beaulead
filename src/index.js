9665efee750ea3f67637cc377abe688c036cf4b6
KIMJUHAN <kjh199201@gaion.kr>
2024-01-04 17:10:42 +0900

    f

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import App from './App';
 import React from 'react';
 import "./scss/common.scss";
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <App />
 );
