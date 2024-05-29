cc130a74702ff507b7fa928369baf8fda2e4a2e8
JUHAN <kjh199201@gmail.com>
2024-05-29 17:41:33 +0900

    init

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 const App = () => {
 
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<div className='main'><p>page1</p></div>} />
-                <Route path="/login" element={<div className='main'><p>page2</p></div>} />
-            </Routes>
-        </BrowserRouter>
+        <React.Fragment>
+            <div>TEST</div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<div className='main'><p>page1</p></div>} />
+                    <Route path="/login" element={<div className='main'><p>page2</p></div>} />
+                </Routes>
+            </BrowserRouter>
+        </React.Fragment>
     );
 };
 
